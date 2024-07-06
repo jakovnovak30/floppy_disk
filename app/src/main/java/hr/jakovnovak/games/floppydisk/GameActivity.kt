79438155ca44fec93502418396173e59ca088350
jakovnovak30 <jakovnovak30@gmail.com>
2024-07-06 14:14:36 +0200

    add new activity

--- app/src/main/java/hr/jakovnovak/games/floppydisk/GameActivity.kt
+++ app/src/main/java/hr/jakovnovak/games/floppydisk/GameActivity.kt
@@ -1,18 +1,21 @@
 package hr.jakovnovak.games.floppydisk
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import hr.jakovnovak.games.floppydisk.ui.main.GameFragment
+import android.view.SurfaceView
 
 class GameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, GameFragment.newInstance())
-                .commitNow()
+
+        val surfaceView = findViewById<SurfaceView>(R.id.surfaceView)
+
+        surfaceView.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/hr/jakovnovak/games/floppydisk/MainActivity.kt
+++ app/src/main/java/hr/jakovnovak/games/floppydisk/MainActivity.kt
@@ -7,21 +7,18 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : Activity() {
-    private var ctr : Int? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val startButton = findViewById<Button>(R.id.playGameButton);
-        val textArea = findViewById<TextView>(R.id.mainMenuTitle);
+        val startButton = findViewById<Button>(R.id.playGameButton)
+        val textArea = findViewById<TextView>(R.id.mainMenuTitle)
 
         startButton.setOnClickListener {
             textArea.text = "Game should start now!"
-            ctr = (ctr ?: 0) + 1
 
-            if ((ctr ?: 0) > 10)
-                textArea.text = "No game for you!"
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
         }
     }
 }
